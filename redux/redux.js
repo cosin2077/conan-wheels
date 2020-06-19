3f522958b095e6f832165d5b57c5fad8c8337d04
conanskyforce <conanskyforce@163.com>
2020-06-19 15:07:43 +0800

    feat: createStore，applyMiddleware

--- redux/redux.js
+++ redux/redux.js
@@ -3,12 +3,50 @@ function compose (...funcs) {
 	if(funcs.length === 1) return funcs[0]
 	return funcs.reduce((x,y)=>(...args) => x(y(...args)))
 }
-
+function applyMiddleware (...middlewares) {
+  return createStore => (...args) => {
+    const store = createStore(...args)
+    let dispatch = () => {}
+    const middlewareApi = {
+      dispatch,
+      getState: store.getState()
+    }
+    const chain = middlewares.map(middleware=>middleware(middlewareApi))
+    dispatch = compose(...chain)(store.dispatch)
+    return {
+      ...store,
+      dispatchs
+    }
+  }
+}
 function createStore (reducer, preState, enhancer) {
-  //TODO
+  if(enhancer && typeof enhancer === 'function') {
+    return enhancer(createStore)(render,preState)
+  }
+  let currentState = preState
+  const listeners = []
+  const getState = () => currentState
+  const subscribe = (fn) => {
+    listeners.push(fn)
+    return function subscribe() {
+      listeners.splice(listeners.indexOf(fn),1)
+    }
+  }
+  const dispatch = (action) => {
+    currentState = reducer(currentState,action)
+    listeners.forEach(listener=>listener())
+    return action
+  }
+  return {
+    subscribe,
+    getState,
+    dispatch
+  }
 }
 
 
 export {
-  compose
+  compose,
+  createStore,
+  applyMiddleware
 }
